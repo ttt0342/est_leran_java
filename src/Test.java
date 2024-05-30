3a0acba770acfac1cfe415d49522bfef63331ed2
seongyeon.kim <ttt0342@naver.com>
2024-05-30 09:34:35 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,34 @@
+public class Test {
+
+
+    /** @author 굿
+     * @deprecated ㅇㅇ (코드는 있으나 사용하지 않을 것을 의미함)
+     * @apiNote 기능설명(무엇을 하는 기능입니다.)  => 자바 독
+     * @since 2024-05-29
+     * @hidden
+     **/
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("GOOD");
+        System.out.println(1);
+
+
+
+        int  a= 10;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+    }
+}
+

2b8a55409666f69892ed4cfce0e3d63608385c77
seongyeon.kim <ttt0342@naver.com>
2024-06-11 18:22:29 +0900

    init

---
+++ src/Java_240611_DailyQuiz.java
@@ -0,0 +1,10 @@
+public class Java_240611_DailyQuiz {
+  public static void main(String[] args) {
+    Java_240611_Me java240611Me = new Java_240611_Me("김성연", 25, "의정부");
+    java240611Me.sayHello();
+
+    Java_240611_Food java240611Food = new Java_240611_Food();
+    java240611Me.eating(java240611Food);
+    java240611Me.cooking(java240611Food);
+  }
+}
---
+++ src/Java_240611_Food.java
@@ -0,0 +1,29 @@
+public class Java_240611_Food {
+  int food;
+
+  public Java_240611_Food() {
+    this.food = 10;
+  }
+
+  public void eatFood() {
+    food--;
+    if (food > 0) {
+      System.out.println("음식을 1개 먹었습니다.");
+      System.out.println("남은 음식: " + food);
+    } else {
+      System.out.println("음식을 다 먹었습니다.");
+      System.out.println("남은 음식: " + food);
+    }
+  }
+
+  public void makeFood() {
+    food++;
+    if (food <= 10) {
+      System.out.println("음식을 만들었습니다.");
+      System.out.println("남은 음식: " + food);
+    } else {
+      System.out.println("음식을 더이상 만들 수 없습니다.");
+      System.out.println("남은 음식: " + food);
+    }
+  }
+}
